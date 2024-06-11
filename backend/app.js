da092262293b68192a317e55ea8d7f63873b69fa
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-06-11 18:30:13 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,6 @@ app.use(express.json())
 app.use(require('./router/auth'))
 const port= process.env.PORT || 5000
 
-app.listen(5000, ()=>{
+app.listen(port, ()=>{
     console.log(`Server is running at ${port}`)
 })
