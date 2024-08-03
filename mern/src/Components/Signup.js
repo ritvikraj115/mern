b0cdf6e9bc67ad20567c882833830deccd81076d
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-08-03 14:29:17 +0530

    Update Signup.js

--- mern/src/Components/Signup.js
+++ mern/src/Components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 
+const backendUrl= process.env.BACKEND_URL;
 const Signup = (props) => {
   const history=useNavigate()
   const [user, setUser]= useState({
