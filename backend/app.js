0bf28ea0a177fdf1c5e2bf8d2dfb8f02a2bf2013
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-08-04 09:44:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,17 @@ const dotenv = require("dotenv")
 const express= require('express');
 const mongoose=require('mongoose')
 const app= express();
+const cors = require("cors");
 dotenv.config({path: './config.env'})
 require('./db/conn')
+app.use(cors());
+app.get('/ok', (req, res) => {
+    res.status(200).send('ok')
+  })
 //const User= require('./model/userSchema')
 app.use(express.json())
 app.use(require('./router/auth'))
-const port= process.env.PORT || 5000
+const port= 3500
 
 app.listen(port, ()=>{
     console.log(`Server is running at ${port}`)
